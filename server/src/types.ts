786a201aef10bf91daf937801835afef72b94fa5
ramoni13 <46489729+ramoni13@users.noreply.github.com>
2026-08-25 10:52:51 +0200

    Update types.ts

--- server/src/types.ts
+++ server/src/types.ts
@@ -282,7 +282,7 @@ export interface PublicRoom {
 export interface ClientEvents {
   create_room: (payload: { pseudo: string; gameOptions?: GameOptions; gameMode?: GameMode }, callback: (res: { roomCode: string; playerId: string } | { error: string }) => void) => void;
   create_solo_room: (payload: { pseudo: string; bots: BotProfile[]; gameOptions?: GameOptions; gameMode?: GameMode }, callback: (res: { roomCode: string; playerId: string } | { error: string }) => void) => void;
-  join_room: (payload: { roomCode: string; pseudo: string }, callback: (res: { playerId: string } | { error: string }) => void) => void;
+  join_room: (payload: { roomCode: string; pseudo: string }, callback: (res: { playerId: string; gameMode: GameMode } | { error: string }) => void) => void;
   player_ready: (callback: (res: { ok: boolean } | { error: string }) => void) => void;
   start_game: (payload: { gameOptions?: GameOptions }, callback: (res: { ok: boolean } | { error: string }) => void) => void;
   yumi_shout: (payload: { clientTimestamp: number }) => void;
